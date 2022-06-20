c8c5a652fa044a69825e5764c42e291afb0d03f9
poldiee <nessidave@gmail.com>
2022-06-20 04:09:38 +0300

    hoods template creaton

--- hoodwatch/views.py
+++ hoodwatch/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from .models import NeighbourHood
 
 
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
-    
+
 def index(request):
     return render(request, 'index.html')
 
@@ -26,3 +27,11 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def hoods(request):
+    all_hoods = NeighbourHood.objects.all()
+
+    params = {
+        'all_hoods': all_hoods
+    }
+    return render(request, 'all_hoods.html', params)
