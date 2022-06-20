0cedb40754ea56c59c6eccbae5ce63d34a8291af
poldiee <nessidave@gmail.com>
2022-06-20 04:47:35 +0300

    newhood creation form

--- hoodwatch/forms.py
+++ hoodwatch/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, NeighbourHood
+from pyuploadcare.dj.forms import ImageField
 
 
 class SignupForm(UserCreationForm):
@@ -15,3 +16,10 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user', 'neighbourhood')
+
+class NeighbourHoodForm(forms.ModelForm):
+    photo = ImageField(label='')
+
+    class Meta:
+        model = NeighbourHood
+        fields = ('logo', 'name', 'location', 'description')
