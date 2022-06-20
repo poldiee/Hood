443ab5a8dd83b5a08a0bb718895ac11551e9f6d6
poldiee <nessidave@gmail.com>
2022-06-20 03:28:20 +0300

    logout button addition for users to logout from app

--- hoodwatch/views.py
+++ hoodwatch/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+    
 def index(request):
     return render(request, 'index.html')
 
