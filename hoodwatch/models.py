6965d8fcb62d254d7afc2c79f9a08595a297c80b
poldiee <nessidave@gmail.com>
2022-06-20 04:00:15 +0300

    model neighbourhood creaton

--- hoodwatch/models.py
+++ hoodwatch/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    logo = models.ImageField(upload_to='images/', default='hoodlogo.png')
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, null=True, on_delete=models.CASCADE, related_name='people')
+
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
