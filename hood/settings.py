157ed822d3219be8a82bd7cf709454451362f2a7
poldiee <nessidave@gmail.com>
2022-06-22 01:18:53 +0300

    margin styling

--- hood/settings.py
+++ hood/settings.py
@@ -41,9 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'hoodwatch',
     'pyuploadcare.dj',
-    'hoodwatch'
 ]
+UPLOADCARE = {
+    'pub_key': 'b1095dbfe67dab73e31d',
+    'secret': 'f9472ff366183cfbc930',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -141,9 +145,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-UPLOADCARE = {
-    'pub_key': 'b1095dbfe67dab73e31d',
-    'secret': 'f9472ff366183cfbc930',
-}
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
